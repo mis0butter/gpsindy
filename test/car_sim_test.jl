27e7e12a72b25ab3c3da8b36953801c08823b395
mis0butter <junetter@gmail.com>
2023-09-09 14:31:44 -0500

    simplified car_sim_test further, removed unnecessary code

--- test/car_sim_test.jl
+++ test/car_sim_test.jl
@@ -10,8 +10,8 @@ data_train, data_test = ode_train_test( fn )
 
 λ = 0.1 
 Ξ_true = SINDy_test( data_train.x_true, data_train.dx_true, λ, data_train.u ) 
-Ξ_true_terms = pretty_coeffs(Ξ_true, data_train.x_true, data_train.u) 
-Ξ_sindy, Ξ_gpsindy, Ξ_gpsindy_x2, Ξ_sindy_terms, Ξ_gpsindy_terms, Ξ_gpsindy_x2_terms = gpsindy_Ξ_fn( data_train.t, data_train.x_true, data_train.dx_true, λ, data_train.u ) 
+Ξ_true_terms = pretty_coeffs(Ξ_true, data_train.x_noise, data_train.u) 
+Ξ_sindy, Ξ_gpsindy, Ξ_gpsindy_x2, Ξ_sindy_terms, Ξ_gpsindy_terms, Ξ_gpsindy_x2_terms = gpsindy_Ξ_fn( data_train.t, data_train.x_noise, data_train.dx_noise, λ, data_train.u ) 
 
 
 ## ============================================ ## 
@@ -47,95 +47,17 @@ push!( xu0, data_train.u[1,1] )
 push!( xu0, data_train.u[1,2] ) 
 dx0_test = dx_fn_sindy( xu0, 0, 0 ) 
 
-x0 = x_test[1,:] 
+x0 = data_test.x_true[1,:] 
 x_unicycle_test   = integrate_euler_unicycle(fn, x0, data_test.t, data_test.u) 
 x_sindy_test      = integrate_euler( dx_fn_sindy, x0, data_test.t, data_test.u) 
 x_gpsindy_test    = integrate_euler( dx_fn_gpsindy, x0, data_test.t, data_test.u) 
 x_gpsindy_x2_test = integrate_euler( dx_fn_gpsindy_x2, x0, data_test.t, data_test.u) 
 
-# x_true_test    = integrate_euler( dx_fn_true, data_test.x_true[1,:], data_test.t, data_test.u ) 
-# x_sindy_test   = integrate_euler( dx_fn_sindy, data_test.x_true[1,:], data_test.t, data_test.u ) 
-# x_gpsindy_test = integrate_euler( dx_fn_gpsindy, data_test.x_true[1,:], data_test.t, data_test.u ) 
-
 t_test = data_test.t 
 
-## ============================================ ##
+# ----------------------- #
 
 # plot smoothed data and validation test data 
 plot_validation_test( t_test, data_test.x_true, x_unicycle_test, x_sindy_test, x_gpsindy_test) 
-
-
-
-## ============================================ ##
-# plot 
-
-using Plots 
-using Latexify
-
-xmin, dx, xmax = min_d_max(t_test)
-
-p_vec = [] 
-for i = 1 : x_vars 
-
-    # ymin, dy, ymax = min_d_max([ x_true_test[:, i]; x_gpsindy_test[:,i] ])
-    ymin = -9 
-    ymax = 2 
-    dy   = 3 
-
-    p = plot( t_test, x_true_test[:,i], 
-        c      = :gray, 
-        label  = "test", 
-        xlabel = "Time (s)", 
-        xticks = xmin:dx:xmax,
-        yticks = ymin:dy:ymax,
-        ylim   = (ymin, ymax), 
-        title  = string(latexify("x_$(i)")),
-    ) 
-    plot!( p, t_test, x_sindy_test[:,i], 
-        c       = :red, 
-        label   = "SINDy", 
-        xticks  = xmin:dx:xmax,
-        yticks  = ymin:dy:ymax,
-        ls      = :dash, 
-        title   = string(latexify("x_$(i)")),
-    ) 
-    plot!( p, t_test, x_gpsindy_test[:,i], 
-        c       = :blue, 
-        label   = "GPSINDy", 
-        xticks  = xmin:dx:xmax,
-        yticks = ymin:dy:ymax,
-        ls      = :dashdot, 
-        title   = string(latexify("x_$(i)")),
-    )
-    push!( p_vec, p ) 
-
-end 
-
-p = deepcopy( p_vec[end] ) 
-plot!( p, 
-    legend = ( -0.1, 0.6 ), 
-    framestyle = :none, 
-    title = "",      
-)  
-push!( p_vec, p ) 
-
-pfig = plot(  p_vec ... , 
-    layout = grid(1, x_vars + 1, widths=[0.2, 0.2, 0.2, 0.2, 0.25]), 
-    size   = [ x_vars * 400 250 ],         
-    margin = 5Plots.mm,
-    bottom_margin = 14Plots.mm,
-)
-
-display(pfig) 
-
-# dt = data_train.t[2] - data_train.t[1] 
-# t_sindy_val,   x_sindy_val   = validate_data( data_test.t, [ data_test.x_noise data_test.u ], dx_sindy_fn, dt ) 
-# t_gpsindy_val, x_gpsindy_val = validate_data( data_test.t, x_GP_train, dx_gpsindy_fn, dt ) 
-
-
-
-
-
-
 
 
