84ba00eaa4e8b3d768f0c4b553312504c39028b4
mis0butter <junetter@gmail.com>
2023-09-09 15:50:37 -0500

    added car_sim_test_compare to compare with car_sim_test from older commit, abfe or something

--- test/car_sim_test.jl
+++ test/car_sim_test.jl
@@ -14,22 +14,6 @@ data_train, data_test = ode_train_test( fn )
 Ξ_sindy, Ξ_gpsindy, Ξ_gpsindy_x2, Ξ_sindy_terms, Ξ_gpsindy_terms, Ξ_gpsindy_x2_terms = gpsindy_Ξ_fn( data_train.t, data_train.x_noise, data_train.dx_noise, λ, data_train.u ) 
 
 
-## ============================================ ## 
-
-Ξ_true = SINDy_test( data_train.x_true, data_train.dx_true, λ, data_train.u ) 
-Ξ_true_terms = pretty_coeffs(Ξ_true, data_train.x_true, data_train.u) 
-
-Ξ_sindy = SINDy_test( data_train.x_true, data_train.dx_true, λ, data_train.u ) 
-Ξ_sindy_terms = pretty_coeffs(Ξ_sindy, data_train.x_true, data_train.u) 
-
-# GPSINDy 
-x_GP  = gp_post( data_train.t, 0*data_train.x_true, data_train.t, 0*data_train.x_noise, data_train.x_true ) 
-dx_GP = gp_post( x_GP, 0*data_train.dx_true, x_GP, 0*data_train.dx_true, data_train.dx_true ) 
-Ξ_gpsindy       = SINDy_test( x_GP, dx_GP, λ, data_train.u ) 
-Ξ_gpsindy_terms = pretty_coeffs(Ξ_gpsindy, x_GP, data_train.u) 
-
-
-
 ## ============================================ ##
 # validate 
 
