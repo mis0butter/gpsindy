1944b94e595967ce0b90eec43dac7275c35e882b
mis0butter <junetter@gmail.com>
2023-09-10 19:37:31 -0500

    adding code to test discovered dynamics on test data

--- test/tune_lambda_test.jl
+++ test/tune_lambda_test.jl
@@ -11,52 +11,60 @@ fn = unicycle
 λ = 0.1 
 data_train, data_test = ode_train_test( fn )
 
-x_train  = data_train.x_noise 
-dx_train = data_train.dx_noise 
-u_train  = data_train.u 
-t_train  = data_train.t 
+x_train_noise  = data_train.x_noise 
+dx_train_noise = data_train.dx_noise 
+u_train        = data_train.u 
+t_train        = data_train.t 
+x_train_true   = data_train.x_true 
+dx_train_true  = data_train.dx_true 
 
-x_true   = data_train.x_true 
-dx_true  = data_train.dx_true 
+x_test_noise   = data_test.x_noise 
+dx_test_noise  = data_test.dx_noise 
+u_test         = data_test.u
+t_test         = data_test.t    
+x_test_true    = data_test.x_true 
+dx_test_true   = data_test.dx_true 
 
 # get sizes 
-x_vars, u_vars, poly_order, n_vars = size_x_n_vars( x_train, u_train ) 
+x_vars, u_vars, poly_order, n_vars = size_x_n_vars( x_train_noise, u_train ) 
 poly_order = 4 
 
-Ξ_true       = sindy_stls( x_true, dx_true, λ, poly_order, u_train ) 
-Ξ_true_terms = pretty_coeffs( Ξ_true, x_true, u_train ) 
+Ξ_true       = sindy_stls( x_train_true, dx_train_true, λ, poly_order, u_train ) 
+Ξ_true_terms = pretty_coeffs( Ξ_true, x_train_true, u_train ) 
 
 
 ## ============================================ ##
 # smooth with GPs 
 
 # first - smooth measurements with Gaussian processes 
-x_GP_train  = gp_post( t_train, 0*x_train, t_train, 0*x_train, x_train ) 
-dx_GP_train = gp_post( x_GP_train, 0*dx_train, x_GP_train, 0*dx_train, dx_train ) 
+x_train_GP  = gp_post( t_train, 0*x_train_noise, t_train, 0*x_train_noise, x_train_noise ) 
+dx_train_GP = gp_post( x_train_GP, 0*dx_train_noise, x_train_GP, 0*dx_train_noise, dx_train_noise ) 
+x_test_GP   = gp_post( t_test, 0*x_test_noise, t_test, 0*x_test_noise, x_test_noise ) 
+dx_test_GP  = gp_post( x_test_GP, 0*dx_test_noise, x_test_GP, 0*dx_test_noise, dx_test_noise ) 
 
 # build function library from smoothed data 
-Θx_gpsindy  = pool_data_test( [ x_GP_train u_train ], n_vars, poly_order ) 
+Θx_gp  = pool_data_test( [ x_train_GP u_train ], n_vars, poly_order ) 
 
 # get x0 from smoothed data 
-x0 = x_GP_train[1,:] 
+x0_train_GP = x_train_GP[1,:] 
 
 # SINDy (STLS) 
 λ = 0.1 
-Θx_sindy      = pool_data_test( [ x_train u_train ], n_vars, poly_order ) 
-Ξ_sindy_stls  = sindy_stls( x_train, dx_train, λ, poly_order, u_train ) 
+Θx_noise      = pool_data_test( [ x_train_noise u_train ], n_vars, poly_order ) 
+Ξ_sindy_stls  = sindy_stls( x_train_noise, dx_train_noise, λ, poly_order, u_train ) 
 dx_fn_sindy   = build_dx_fn( poly_order, x_vars, u_vars, Ξ_sindy_stls ) 
-dx_sindy      = Θx_sindy * Ξ_sindy_stls  
-x_sindy_train = integrate_euler( dx_fn_sindy, x0, t_train, u_train ) 
+dx_sindy      = Θx_noise * Ξ_sindy_stls  
+x_train_sindy = integrate_euler( dx_fn_sindy, x0_train_GP, t_train, u_train ) 
 
 # integrate true function 
 dx_fn_true    = build_dx_fn( poly_order, x_vars, u_vars, Ξ_true ) 
-x_true_train  = integrate_euler( dx_fn_true, x0, t_train, u_train ) 
+x_train_true  = integrate_euler( dx_fn_true, x0_train_GP, t_train, u_train ) 
 
 # integrate unicycle (with GP) 
-x_unicycle_train = integrate_euler_unicycle( fn, x0, t_train, u_train ) 
+x_unicycle_train = integrate_euler_unicycle( fn, x0_train_GP, t_train, u_train ) 
 for i = 1 : x_vars 
-    println( "dx_fn_true norm err = ", norm( x_true_train[:,i] - x_true[:,i] )  ) 
-    println( "unicycle norm err = ", norm( x_unicycle_train[:,i] - x_true[:,i] )  ) 
+    println( "dx_fn_true norm err = ", norm( x_train_true[:,i] - x_train_true[:,i] )  ) 
+    println( "unicycle norm err = ", norm( x_unicycle_train[:,i] - x_train_true[:,i] )  ) 
 end 
 
 ## ============================================ ##
@@ -80,8 +88,8 @@ for j = 1 : x_vars
     a_x = Animation() 
 
     # DX PLOT 
-    ymin, dy, ymax = min_d_max( dx_true[:,j] ) 
-    plt_dx = plot( t_train, dx_true[:,j], 
+    ymin, dy, ymax = min_d_max( dx_train_true[:,j] ) 
+    plt_dx = plot( t_train, dx_train_true[:,j], 
         label = "true", 
         xlabel = "t (s)", 
         ylabel = "dx", 
@@ -90,11 +98,11 @@ for j = 1 : x_vars
         legend = :outerright,
         size = (1000, 400), 
     ) ; frame(a_dx, plt_dx) 
-    plot!( plt_dx, t_train, dx_train[:,j], 
+    plot!( plt_dx, t_train, dx_train_noise[:,j], 
         label = "noise", 
         ls    = :dash,  
     ) ; frame(a_dx, plt_dx) 
-        plot!( plt_dx, t_train, dx_GP_train[:,j], 
+        plot!( plt_dx, t_train, dx_train_GP[:,j], 
         label = "GP", 
         ls    = :dashdot,   
 
@@ -105,8 +113,8 @@ for j = 1 : x_vars
     ) ; frame(a_dx, plt_dx) 
 
     # X PLOT 
-    ymin, dy, ymax = min_d_max( x_true[:,j] ) 
-    plt_x = plot( t_train, x_true[:,j], 
+    ymin, dy, ymax = min_d_max( x_train_true[:,j] ) 
+    plt_x = plot( t_train, x_train_true[:,j], 
         label = "true", 
         xlabel = "t (s)", 
         ylabel = "x", 
@@ -115,11 +123,11 @@ for j = 1 : x_vars
         legend = :outerright,
         size = (1000, 400), 
     ) 
-    plot!( plt_x, t_train, x_train[:,j], 
+    plot!( plt_x, t_train, x_train_noise[:,j], 
         label = "noise", 
         ls    = :dash,  
     ) ; frame(a_x, plt_x) 
-    plot!( plt_x, t_train, x_GP_train[:,j], 
+    plot!( plt_x, t_train, x_train_GP[:,j], 
         label = "GP", 
         ls    = :dashdot,   
     ) ; frame(a_x, plt_x) 
@@ -128,7 +136,7 @@ for j = 1 : x_vars
         ls    = :dashdotdot,   
     ) ; frame(a_x, plt_x) 
     plt_x_sindy = deepcopy(plt_x) 
-    plot!( plt_x_sindy, t_train, x_sindy_train[:,j], 
+    plot!( plt_x_sindy, t_train, x_train_sindy[:,j], 
         label = "SINDy",
         ls    = :dot,  
     ) ; frame(a_x, plt_x_sindy) 
@@ -145,21 +153,21 @@ for j = 1 : x_vars
         λ = λ_vec[i] 
         println( "i = ", i, ". λ = ", λ ) 
 
-        Ξ_gpsindy  = sindy_lasso( x_GP_train, dx_GP_train, λ, poly_order, u_train ) 
-        dx_gpsindy = Θx_gpsindy * Ξ_gpsindy 
+        Ξ_gpsindy  = sindy_lasso( x_train_GP, dx_train_GP, λ, poly_order, u_train ) 
+        dx_gpsindy = Θx_gp * Ξ_gpsindy 
 
         if sum(Ξ_gpsindy[:,j]) == 0 
             break 
         end 
 
-        Ξ_gpsindy_terms = pretty_coeffs( Ξ_gpsindy, x_GP_train, u_train ) 
+        Ξ_gpsindy_terms = pretty_coeffs( Ξ_gpsindy, x_train_GP, u_train ) 
         display(Ξ_gpsindy_terms) 
 
         dx_fn_gpsindy   = build_dx_fn( poly_order, x_vars, u_vars, Ξ_gpsindy ) 
-        x_gpsindy_train = integrate_euler( dx_fn_gpsindy, x0, t_train, u_train ) 
+        x_gpsindy_train = integrate_euler( dx_fn_gpsindy, x0_train_GP, t_train, u_train ) 
         err_ξ_norm      = norm( Ξ_true[:,j] - Ξ_gpsindy[:,j] ) 
-        err_xj_norm     = norm( x_gpsindy_train[:,j] - x_GP_train[:,j] ) 
-        err_dxj_norm    = norm( dx_gpsindy[:,j] - dx_GP_train[:,j] ) 
+        err_xj_norm     = norm( x_gpsindy_train[:,j] - x_train_GP[:,j] ) 
+        err_dxj_norm    = norm( dx_gpsindy[:,j] - dx_train_GP[:,j] ) 
 
         # plot_validation_test( t_train, x_train, x_unicycle_train, x_sindy_train, x_gpsindy_train ) 
 
@@ -221,13 +229,14 @@ for i = 1 : x_vars
 
 end 
 
+# integrate with ξ with smallest x error for training data 
 dx_fn_gpsindy_minerr = build_dx_fn( poly_order, x_vars, u_vars, Ξ_gpsindy_minerr ) 
-x_gpsindy_train      = integrate_euler( dx_fn_gpsindy_minerr, x0, t_train, u_train ) 
+x_gpsindy_train      = integrate_euler( dx_fn_gpsindy_minerr, x0_train_GP, t_train, u_train ) 
 
 plt_x_vec = [] 
 for j = 1 : x_vars 
-    ymin, dy, ymax = min_d_max( x_true[:,j] ) 
-    plt_x = plot( t_train, x_true[:,j], 
+    ymin, dy, ymax = min_d_max( x_train_true[:,j] ) 
+    plt_x = plot( t_train, x_train_true[:,j], 
         label = "true", 
         xlabel = "t (s)", 
         ylabel = "x", 
@@ -236,11 +245,11 @@ for j = 1 : x_vars
         legend = :outerright,
         size = (1000, 400), 
     ) 
-    plot!( plt_x, t_train, x_train[:,j], 
+    plot!( plt_x, t_train, x_train_noise[:,j], 
         label = "noise", 
         ls    = :dash,  
     ) 
-    plot!( plt_x, t_train, x_GP_train[:,j], 
+    plot!( plt_x, t_train, x_train_GP[:,j], 
         label = "GP", 
         ls    = :dashdot,   
     ) 
@@ -259,3 +268,6 @@ pfig = plot( plt_x_vec ... ,
     size = (1000, 400*x_vars), 
     plot_title = "training data", 
 )
+
+# integrate with ξ with smallest x error for test data 
+x_gpsindy_test = integrate_euler( dx_fn_gpsindy_minerr, x0, t_test, u_test )
