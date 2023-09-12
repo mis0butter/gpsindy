68cdcf3acd897559eb1dcd60baf2a8a08d23a806
Junette Hsin <junetter@gmail.com>
2023-09-12 13:14:02 -0500

    pretty_coeffs disappeared? re-added with bug fixgit add .

--- src/SINDy_test.jl
+++ src/SINDy_test.jl
@@ -1,103 +1,38 @@
 ## ============================================ ##
 # putting it together (no control) 
 
-export sindy_stls 
-function sindy_stls( x, dx, λ, u = false )
+export SINDy_test 
+function SINDy_test( x, dx, λ )
 
-    x_vars, u_vars, poly_order, n_vars = size_x_n_vars( dx, u ) 
-    
-    if isequal(u, false)      # if u_data = false 
-        data   = x 
-    else            # there are u_data inputs 
-        data   = [ x u ]
-    end 
+    n_vars = size(x, 2) 
+    poly_order = n_vars 
 
     # construct data library 
-    Θx = pool_data_test(data, n_vars, poly_order) 
+    Θx = pool_data_test(x, n_vars, poly_order) 
 
-    # SINDy 
-    Ξ = sparsify_dynamics_stls( Θx, dx, λ, x_vars ) 
+    # first cut - SINDy 
+    Ξ = sparsify_dynamics_test(Θx, dx, λ, n_vars) 
 
     return Ξ
 
 end 
 
 ## ============================================ ##
-# putting it together (no control) 
+# putting it together (with control) 
 
-export sindy_lasso 
-function sindy_lasso( x, dx, λ, u = false )
+export SINDy_c_test 
+function SINDy_c_test( x, u, dx, λ )
 
-    x_vars, u_vars, poly_order, n_vars = size_x_n_vars( dx, u ) 
-    
-    if isequal(u, false)      # if u_data = false 
-        data = x 
-    else            # there are u_data inputs 
-        data = [ x u ] 
-    end 
+    n_vars = size( [x u], 2 )
+    x_vars = size(x, 2)
+    u_vars = size(u, 2) 
+    poly_order = n_vars 
 
     # construct data library 
-    Θx = pool_data_test(data, n_vars, poly_order) 
+    Θx = pool_data_test( [x u], n_vars, poly_order) 
 
-    # SINDy 
-    Ξ = sparsify_dynamics_lasso( Θx, dx, λ, x_vars ) 
-
-    return Ξ
-
-end 
-
-
-# ## ============================================ ##
-# # putting it together (with control) 
-
-# export SINDy_c_test 
-# function SINDy_c_test( x, u, dx, λ )
-
-#     x_vars = size(x, 2)
-#     u_vars = size(u, 2) 
-#     n_vars = x_vars + u_vars 
-#     poly_order = x_vars 
-
-#     # construct data library 
-#     Θx = pool_data_test( [x u], n_vars, poly_order ) 
-
-#     # first cut - SINDy 
-#     # Ξ = sparsify_dynamics_stls( Θx, dx, λ, x_vars ) 
-#     Ξ = sparsify_dynamics_cstrnd( Θx, dx, λ, x_vars ) 
-
-#     return Ξ 
-
-# end 
-
-
-## ============================================ ##
-# solve sparse regression 
-
-export sparsify_dynamics_lasso 
-function sparsify_dynamics_lasso( Θx, dx, λ, n_vars ) 
-# ----------------------- #
-# Purpose: Solve for active terms in dynamics through sparse regression 
-# 
-# Inputs: 
-#   Θx     = data matrix (of input states) 
-#   dx     = state derivatives 
-#   lambda = sparsification knob (threshold) 
-#   n_vars = # elements in state 
-# 
-# Outputs: 
-#   Ξ      = sparse coefficients of dynamics 
-# ----------------------- #
-
-    # first perform least squares 
-    Ξ = Θx \ dx 
-
-    # for each element in state 
-    for j = 1 : n_vars 
-
-        x, z, hist = lasso_admm( Θx, dx[:,j], λ ) 
-        Ξ[:, j]    = z  
-
-    end 
+    # first cut - SINDy 
+    Ξ = sparsify_dynamics_test( Θx, dx, λ, x_vars ) 
 
     return Ξ
 
@@ -107,8 +42,8 @@ end
 ## ============================================ ##
 # solve sparse regression 
 
-export sparsify_dynamics_stls 
-function sparsify_dynamics_stls( Θx, dx, λ, n_vars ) 
+export sparsify_dynamics_test 
+function sparsify_dynamics_test( Θx, dx, λ, n_vars ) 
 # ----------------------- #
 # Purpose: Solve for active terms in dynamics through sparse regression 
 # 
@@ -141,84 +76,7 @@ function sparsify_dynamics_stls( Θx, dx, λ, n_vars )
             big_inds = findall( >=(λ), abs.( Ξ[:,j] ) ) 
 
             # regress dynamics onto remaining terms to find sparse Ξ
-            Ξ[big_inds, j] = Θx[:, big_inds] \ dx[:,j] 
-
-        end 
-
-    end 
-    
-    return Ξ
-end 
-
-
-## ============================================ ##
-# define min function 
-
-using Optim 
-
-export min_Ax_b 
-function min_Ax_b( A, b, bound = 10 ) 
-
-    x0 = 0 * ( A \ b ) 
-
-    # Optim stuff 
-    upper = bound * ones(size(x0)) 
-    lower = -upper 
-    f_test(x) = 1/2 * norm(A*x - b)^2 
-
-    # optimization 
-    od     = OnceDifferentiable( f_test, x0 ; autodiff = :forward ) 
-    result = optimize( od, lower, upper, x0, Fminbox(LBFGS()) ) 
-    x_opt  = result.minimizer 
-
-    return x_opt 
-
-end 
-
-
-## ============================================ ##
-# solve sparse regression 
-
-using Optim 
-
-export sparsify_dynamics_cstrnd 
-function sparsify_dynamics_cstrnd( Θx, dx, λ, x_vars ) 
-# ----------------------- #
-# Purpose: Solve for active terms in dynamics through sparse CONSTRAINED regression 
-# 
-# Inputs: 
-#   Θx     = data matrix (of input states) 
-#   dx     = state derivatives 
-#   lambda = sparsification knob (threshold) 
-#   n_vars = # elements in state 
-# 
-# Outputs: 
-#   Ξ      = sparse coefficients of dynamics 
-# ----------------------- #
-
-    # first perform least squares 
-    Ξ = Θx \ dx 
-
-    # sequentially thresholded least squares = LASSO. Do 10 iterations 
-    for k = 1 : 10 
-
-        # for each element in state 
-        for j = 1 : x_vars 
-
-            # small_inds = rows of |Ξ| < λ
-            small_inds = findall( <(λ), abs.(Ξ[:,j]) ) 
-
-            # set elements < λ to 0 
-            Ξ[small_inds, j] .= 0 
-
-            # big_inds --> select columns of Θx
-            big_inds = findall( >=(λ), abs.( Ξ[:,j] ) ) 
-
-            # regress dynamics onto remaining terms to find sparse Ξ
-            A     = Θx[:, big_inds] 
-            b     = dx[:,j] 
-            x_opt = min_Ax_b( A, b ) 
-            Ξ[big_inds, j] = x_opt 
+            Ξ[big_inds, j] = Θx[:, big_inds] \ dx[:,j]
 
         end 
 
@@ -227,7 +85,6 @@ function sparsify_dynamics_cstrnd( Θx, dx, λ, x_vars )
     return Ξ
 
 end 
-
 
 ## ============================================ ##
 # build data matrix 
@@ -253,20 +110,29 @@ function pool_data_test(xmat, n_vars, poly_order)
     # # fill out 1st column of Θx with ones (poly order = 0) 
     ind = 1 ; 
     Θx  = ones(l, ind) 
-    
+
     # poly order 1 
     for i = 1 : n_vars 
         ind += 1 
         Θx   = [ Θx xmat[:,i] ]
     end 
+    # Θx = xmat[:,1]
+    # Θx = Θx[:,:] 
+    # Θx = [ xmat[:,1] xmat[:,2] ]
+
+    # ind += 1 
+    # vec  = xmat[:,1] .* xmat[:,2]
+    # Θx   = [ Θx vec[:,:] ]
 
     # poly order 2 
     if poly_order >= 2 
         for i = 1 : n_vars 
-            for j = i : n_vars 
+            for j = i:n_vars 
+
                 ind += 1 ; 
                 vec  = xmat[:,i] .* xmat[:,j] 
                 Θx   = [Θx vec] 
+
             end 
         end 
     end 
@@ -276,9 +142,11 @@ function pool_data_test(xmat, n_vars, poly_order)
         for i = 1 : n_vars 
             for j = i : n_vars 
                 for k = j : n_vars 
+                    
                     ind += 1 ;                     
                     vec  = xmat[:,i] .* xmat[:,j] .* xmat[:,k] 
                     Θx   = [Θx vec] 
+
                 end 
             end 
         end 
@@ -298,50 +166,8 @@ function pool_data_test(xmat, n_vars, poly_order)
         Θx    = [Θx vec] 
     end 
 
-    # nonlinear combination with sine functions 
-    for i = 1 : n_vars 
-        for j = 1 : n_vars 
-            ind  += 1 
-            vec   = xmat[:,i] .* sin.(xmat[:,j]) 
-            Θx    = [Θx vec]     
-        end 
-    end 
-
-    # nonlinear combination with cosine functions 
-    for i = 1 : n_vars 
-        for j = 1 : n_vars 
-            ind  += 1 
-            vec   = xmat[:,i] .* cos.(xmat[:,j]) 
-            Θx    = [Θx vec]     
-        end 
-    end 
-
-    # poly order 2 nonlinear combination with sine functions 
-    if poly_order >= 2 
-        for i = 1 : n_vars 
-            for j = i : n_vars 
-                for k = 1 : n_vars 
-                    ind  += 1 
-                    vec   = xmat[:,i] .* xmat[:,j] .* sin.(xmat[:,k]) 
-                    Θx    = [Θx vec]     
-                end
-            end 
-        end 
-    end 
-
-    # poly order 2 nonlinear combination with cosine functions 
-    if poly_order >= 2 
-        for i = 1 : n_vars 
-            for j = i : n_vars 
-                for k = 1 : n_vars 
-                    ind  += 1 
-                    vec   = xmat[:,i] .* xmat[:,j] .* cos.(xmat[:,k]) 
-                    Θx    = [Θx vec]     
-                end
-            end 
-        end 
-    end 
-
+    
+    
     return Θx  
 
 end 
@@ -352,28 +178,30 @@ end
 
 export pool_data_vecfn_test
 function pool_data_vecfn_test(n_vars, poly_order) 
-# ----------------------- #
-# Purpose: Build data vector of functions  
-# 
-# Inputs: 
-#   n_vars      = # elements in state 
-#   poly_order  = polynomial order (goes up to order 3) 
-# 
-# Outputs: 
-#   Θ       = data matrix passed through function library 
-# ----------------------- #
+    # ----------------------- #
+    # Purpose: Build data vector of functions  
+    # 
+    # Inputs: 
+    #   n_vars      = # elements in state 
+    #   poly_order  = polynomial order (goes up to order 3) 
+    # 
+    # Outputs: 
+    #   Θ       = data matrix passed through function library 
+    # ----------------------- #
     
     # initialize empty vector of functions 
-    Θx = Vector{Function}(undef,0) 
+    Θ = Vector{Function}(undef,0) 
 
     # fil out 1st column of Θ with ones (poly order = 0) 
     ind  = 1 
-    push!(Θx, x -> 1) 
+    push!(Θ, x -> 1) 
 
     # poly order 1 
     for i = 1 : n_vars 
+
         ind  += 1 
-        push!( Θx, x -> x[i] ) 
+        push!( Θ, x -> x[i] ) 
+
     end 
 
     # ind += 1 
@@ -383,8 +211,10 @@ function pool_data_vecfn_test(n_vars, poly_order)
     if poly_order >= 2 
         for i = 1 : n_vars 
             for j = i:n_vars 
+
                 ind += 1 ; 
-                push!( Θx, x -> x[i] .* x[j] ) 
+                push!( Θ, x -> x[i] .* x[j] ) 
+
             end 
         end 
     end 
@@ -394,8 +224,10 @@ function pool_data_vecfn_test(n_vars, poly_order)
         for i = 1 : n_vars 
             for j = i : n_vars 
                 for k = j : n_vars 
+                    
                     ind += 1 ;                     
-                    push!( Θx, x -> x[i] .* x[j] .* x[k] )
+                    push!( Θ, x -> x[i] .* x[j] .* x[k] )
+
                 end 
             end 
         end 
@@ -403,169 +235,33 @@ function pool_data_vecfn_test(n_vars, poly_order)
 
     # sine functions 
     for i = 1 : n_vars 
+
         ind  += 1
-        push!(Θx, x -> sin.( x[i] ) )
+        push!(Θ, x -> sin.( x[i] ) )
+
     end 
 
     # sine functions 
     for i = 1 : n_vars 
+
         ind  += 1
-        push!(Θx, x -> cos.( x[i] ) ) 
-    end 
+        push!(Θ, x -> cos.( x[i] ) )
 
-    # nonlinear combinations with sine functions 
-    for i = 1 : n_vars 
-        for j = 1 : n_vars 
-            ind += 1 
-            push!( Θx, x -> x[i] .* sin.( x[j] ) ) 
-        end 
     end 
-
-    # nonlinear combinations with cosine functions 
-    for i = 1 : n_vars 
-        for j = 1 : n_vars 
-            ind += 1 
-            push!( Θx, x -> x[i] .* cos.( x[j] ) ) 
-        end 
-    end 
-
-    # poly order 2 nonlinear combination with sine functions 
-    if poly_order >= 2 
-        for i = 1 : n_vars 
-            for j = i : n_vars 
-                for k = 1 : n_vars 
-                    ind += 1 
-                    push!( Θx, x -> x[i] .* x[j] .* sin.( x[k] ) ) 
-                end
-            end 
-        end 
-    end 
-
-    # poly order 2 nonlinear combination with cosine functions 
-    if poly_order >= 2 
-        for i = 1 : n_vars 
-            for j = i : n_vars 
-                for k = 1 : n_vars 
-                    ind += 1 
-                    push!( Θx, x -> x[i] .* x[j] .* cos.( x[k] ) ) 
-                end
-            end 
-        end 
-    end
     
-    return Θx 
+    return Θ 
 
 end 
 
-
-## ============================================ ##
-# export terms (with control)
-
-export nonlinear_terms 
-function nonlinear_terms( x_data, u_data = false ) 
-
-    terms = [] 
-    x_vars = size(x_data, 2) 
-    u_vars = size(u_data, 2) 
-
-    var_string = [] 
-    for i = 1 : x_vars 
-        push!( var_string, string("x", i) )
-    end  
-    if isequal(u_data, false)      # if u_data = false 
-        n_vars = x_vars 
-    else            # there are u_data inputs 
-        n_vars = x_vars + u_vars 
-        for i = 1 : u_vars 
-            push!( var_string, string("u", i) ) 
-        end 
-    end 
-    
-    # first one 
-    ind = 1  
-    push!( terms, 1 )
-    
-     # poly order 1 
-    for i = 1 : n_vars 
-        ind += 1 
-        push!( terms, var_string[i] ) 
-    end 
-    
-    # poly order 2 
-    for i = 1 : n_vars 
-        for j = i : n_vars 
-            ind += 1 
-            push!( terms, string( var_string[i], var_string[j] ) ) 
-        end 
-    end 
-    
-     # poly order 3 
-    for i = 1 : n_vars 
-        for j = i : n_vars 
-            for k = j : n_vars 
-                ind += 1 
-                push!( terms, string( var_string[i], var_string[j], var_string[k] ) )     
-            end 
-        end 
-    end 
-    
-     # sine functions 
-    for i = 1 : n_vars 
-        ind += 1 
-        push!( terms, string( "sin(", var_string[i], ")" ) ) 
-    end 
-    
-     for i = 1 : n_vars 
-        ind += 1 
-        push!( terms, string( "cos(", var_string[i], ")" ) ) 
-    end 
-    
-     # cosine functions 
-    for i = 1 : n_vars 
-        for j = 1 : n_vars 
-            ind += 1 
-            push!( terms, string( var_string[i], "sin(", var_string[j], ")") ) 
-        end 
-    end 
-    
-     for i = 1 : n_vars 
-        for j = 1 : n_vars 
-            ind += 1 
-            push!( terms, string( var_string[i], "cos(", var_string[j], ")") ) 
-        end 
-    end 
-
-    # poly order 2 nonlinear combination with sine functions 
-    for i = 1 : n_vars 
-        for j = i : n_vars 
-            for k = 1 : n_vars 
-                ind += 1 
-                push!( terms, string( var_string[i], var_string[j], "sin(", var_string[k], ")") ) 
-            end
-        end 
-    end 
-
-    # poly order 2 nonlinear combination with cosine functions 
-    for i = 1 : n_vars 
-        for j = i : n_vars 
-            for k = 1 : n_vars 
-                ind += 1 
-                push!( terms, string( var_string[i], var_string[j], "cos(", var_string[k], ")") ) 
-            end
-        end 
-    end 
-
-    return terms 
-end 
 
 
 ## ============================================ ##
 
 export pretty_coeffs 
-function pretty_coeffs(Ξ_true, x_true, u = false)
+function pretty_coeffs(Ξ_true, x_true, u = false, poly_order = 1)
 
     # compute all nonlinear terms 
-    terms = nonlinear_terms( x_true, u ) 
+    terms = nonlinear_terms( x_true, u, poly_order ) 
 
     # header 
     n_vars = size(x_true, 2) 
@@ -579,7 +275,7 @@ function pretty_coeffs(Ξ_true, x_true, u = false)
     n_vars = size(x_true, 2) 
     inds = [] 
     for i = 1 : n_vars 
-        ind  = findall( x -> x > 0, Ξ_true[:,i] ) 
+        ind  = findall( x -> abs(x) > 0, Ξ_true[:,i] ) 
         inds = [ inds ; ind ]
     end 
     inds = sort(unique(inds)) 
@@ -598,5 +294,4 @@ function pretty_coeffs(Ξ_true, x_true, u = false)
     return Ξ_terms 
 
 end 
-
 
--- test/tune_lambda_car_hardware_test.jl
+++ test/tune_lambda_car_hardware_test.jl
@@ -1,5 +1,5 @@
 using GaussianSINDy 
-
+using LinearAlgebra 
 
 ## ============================================ ##
 # single run (good) 
@@ -10,26 +10,16 @@ for i in eachindex(csv_files_vec)
     csv_files_vec[i] = string( path, csv_files_vec[i] ) 
 end
 
-Ξ_gpsindy = [] 
-x_gpsindy = [] 
-x_sindy   = [] 
-# for i = eachindex(csv_files_vec) 
-# for i = [ 4 ]
-    i = 4 
+x_err_hist  = x_err_struct([], [], [], [])
+for i = eachindex(csv_files_vec) 
+# for i = [ 4, 5 ]
+    # i = 4 
     csv_file = csv_files_vec[i] 
-    t_train, t_test, x_train_noise, x_test_noise, Ξ_sindy_stls, x_train_sindy, x_test_sindy, Ξ_gpsindy_minerr, x_train_gpsindy, x_test_gpsindy = cross_validate_gpsindy( csv_file, 1 )
-# end 
+    t_train, t_test, x_train_noise, x_test_noise, Ξ_sindy_stls, x_train_sindy, x_test_sindy, Ξ_gpsindy_minerr, x_train_gpsindy, x_test_gpsindy = cross_validate_gpsindy( csv_file, 1 ) 
 
-
-## ============================================ ##
-# save outputs as csv 
-header = [ "t", "x1_sindy", "x2_sindy", "x3_sindy", "x4_sindy", "x1_gpsindy", "x2_gpsindy", "x3_gpsindy", "x4_gpsindy" ] 
-
-data   = [ t_test x_test_sindy x_test_gpsindy ]
-df     = DataFrame( data,  :auto ) 
-CSV.write(string("car_hw_single", ".csv"), df, header=header)
-
-
+    push!( x_err_hist.sindy_lasso, norm( x_test_noise - x_test_sindy )  ) 
+    push!( x_err_hist.gpsindy,     norm( x_test_noise - x_test_gpsindy )  ) 
+end 
 
 ## ============================================ ## 
 # single run (good) 
@@ -49,6 +39,15 @@ i = 4
     csv_file = csv_files_vec[i] 
     t_train, t_test, x_train_noise, x_test_noise, Ξ_sindy_stls, x_train_sindy, x_test_sindy, Ξ_gpsindy_minerr, x_train_gpsindy, x_test_gpsindy = cross_validate_gpsindy( csv_file, 1 )
 # end 
+
+
+## ============================================ ##
+# save outputs as csv 
+header = [ "t", "x1_sindy", "x2_sindy", "x3_sindy", "x4_sindy", "x1_gpsindy", "x2_gpsindy", "x3_gpsindy", "x4_gpsindy" ] 
+
+data   = [ t_test x_test_sindy x_test_gpsindy ]
+df     = DataFrame( data,  :auto ) 
+CSV.write(string("car_hw_single", ".csv"), df, header=header)
 
 
 
