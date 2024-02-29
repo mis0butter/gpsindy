b77ea642ca6f84a928f443361a35b2df087e47a4
mis0butter <junetter@gmail.com>
2024-02-29 13:57:15 -0600

    adjusted control input, saved as csvs

--- test/tune_lambda_car_hardware_test.jl
+++ test/tune_lambda_car_hardware_test.jl
@@ -22,6 +22,51 @@ for i = eachindex(csv_files_vec)
     push!( x_err_hist.gpsindy,     norm( x_test_noise - x_test_gpsindy )  ) 
 end 
 
+
+## ============================================ ##
+# single run (good) 
+
+path          = "test/data/jake_car_csvs/" 
+csv_files_vec = readdir( path ) 
+for i in eachindex(csv_files_vec)  
+    csv_files_vec[i] = string( path, csv_files_vec[i] ) 
+end 
+
+x_err_hist  = x_err_struct([], [], [], [])
+for i = eachindex(csv_files_vec) 
+# for i = [ 4 ]
+    # i = 4 
+    csv_file = csv_files_vec[i] 
+    t_train, t_test, x_train_noise, x_test_noise, Ξ_sindy_stls, x_train_sindy, x_test_sindy, Ξ_gpsindy_minerr, x_train_gpsindy, x_test_gpsindy = cross_validate_gpsindy( csv_file, 1 ) 
+
+    push!( x_err_hist.sindy_lasso, norm( x_test_noise - x_test_sindy )  ) 
+    push!( x_err_hist.gpsindy,     norm( x_test_noise - x_test_gpsindy )  ) 
+end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ## ============================================ ## 
 # single run (good) 
 
