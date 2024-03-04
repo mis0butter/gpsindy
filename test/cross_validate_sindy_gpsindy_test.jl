fb6b1ee56f73734621a56ebaf14126cfa4486e6a
Junette Hsin <junetter@gmail.com>
2024-03-04 17:59:43 -0600

    committing labeled data for Somi

--- test/cross_validate_sindy_gpsindy_test.jl
+++ test/cross_validate_sindy_gpsindy_test.jl
@@ -6,8 +6,8 @@ using Statistics
 ## ============================================ ##
 # controls adjust sparse 5 hz 
 
-csv_path = "test/data/jake_car_csvs_control_adjust_50hz_noise_0.1/" 
-img_path = "test/images/50hz_noise_0.1/" 
+csv_path = "test/data/jake_car_csvs_control_adjust_50hz_noise_0.01/" 
+img_path = "test/images/50hz_noise_0.01/" 
 
 csv_files_vec = readdir( csv_path ) 
 for i in eachindex(csv_files_vec)  
@@ -49,50 +49,10 @@ sindy_5hz_3sigma   = reject_outliers( x_err_hist_5hz.sindy_lasso )
 gpsindy_5hz_3sigma = reject_outliers( x_err_hist_5hz.gpsindy ) 
 
 
-## ============================================ ##
-# controls adjust sparse 5 hz 
 
-csv_path = "test/data/jake_car_csvs_control_adjust_10hz/" 
-img_path = "test/images/10hz/" 
 
-csv_files_vec = readdir( csv_path ) 
-for i in eachindex(csv_files_vec)  
-    csv_files_vec[i] = string( csv_path, csv_files_vec[i] ) 
-end 
 
-# check if save_path exists 
-if !isdir( img_path ) 
-    mkdir( img_path ) 
-end 
 
-x_err_hist_5hz  = x_err_struct( [], [], [], [] ) 
-for i = eachindex( csv_files_vec ) 
-# for i = [ 4 ]
-    # i = 42 
-    csv_file = csv_files_vec[i] 
-    t_train, t_test, x_train_noise, x_test_noise, Ξ_sindy_stls, x_train_sindy, x_test_sindy, Ξ_gpsindy_minerr, x_train_gpsindy, x_test_gpsindy, fig_train, fig_test = cross_validate_sindy_gpsindy( csv_file, 1 ) 
-
-    # save fig_train 
-    fig_train_save = replace( csv_file, csv_path => img_path )  
-    fig_train_save = replace( fig_train_save, ".csv" => "_train.png" ) 
-    save( fig_train_save, fig_train ) 
-
-    # save fig_test 
-    fig_test_save = replace( csv_file, csv_path => img_path )  
-    fig_test_save = replace( fig_test_save, ".csv" => "_test.png" ) 
-    save( fig_test_save, fig_test ) 
-    
-    push!( x_err_hist_5hz.sindy_lasso, norm( x_test_noise - x_test_sindy )  ) 
-    push!( x_err_hist_5hz.gpsindy,     norm( x_test_noise - x_test_gpsindy )  ) 
-
-end 
-
-# find index that is equal to maximum 
-findall( x_err_hist_5hz.sindy_lasso .== maximum( x_err_hist_5hz.sindy_lasso ) ) 
-
-# reject 3-sigma outliers 
-sindy_5hz_3sigma   = reject_outliers( x_err_hist_5hz.sindy_lasso ) 
-gpsindy_5hz_3sigma = reject_outliers( x_err_hist_5hz.gpsindy ) 
 
 
 
@@ -102,10 +62,19 @@ gpsindy_5hz_3sigma = reject_outliers( x_err_hist_5hz.gpsindy )
 ## ============================================ ##
 # test cross_validate_sindy_gpsindy  
 
+csv_file = "test/data/jake_car_csvs_control_adjust_50hz_noise_0.01/rollout_shift_19_noise.csv" 
+
 data_train, data_test = car_data_struct( csv_file ) 
 
 # smooth with GPs 
-x_train_GP, dx_train_GP, x_test_GP, dx_test_GP = gp_train_test( data_train, data_test ) 
+# x_train_GP, dx_train_GP, x_test_GP, dx_test_GP = gp_train_test( data_train, data_test ) 
+# ----------------------- #
+# function gp_train_test 
+# first - smooth measurements with Gaussian processes 
+x_train_GP  = gp_post( data_train.t, 0*data_train.x_noise, data_train.t, 0*data_train.x_noise, data_train.x_noise ) 
+dx_train_GP = gp_post( x_train_GP, 0*data_train.dx_noise, x_train_GP, 0*data_train.dx_noise, data_train.dx_noise ) 
+x_test_GP   = gp_post( data_test.t, 0*data_test.x_noise, data_test.t, 0*data_test.x_noise, data_test.x_noise ) 
+dx_test_GP  = gp_post( x_test_GP, 0*data_test.dx_noise, x_test_GP, 0*data_test.dx_noise, data_test.dx_noise ) 
 
 # get x0 from smoothed data 
 x0_train_GP = x_train_GP[1,:] 
@@ -114,6 +83,11 @@ x0_test_GP  = x_test_GP[1,:]
 # cross-validate SINDy!!! 
 λ_vec = λ_vec_fn() 
 # Ξ_sindy_vec, err_x_sindy, err_dx_sindy = cross_validate_λ( data_train.t, data_train.x_noise, data_train.dx_noise, data_train.u, λ_vec ) 
+
+
+
+## ============================================ ##
+
 
 
 
