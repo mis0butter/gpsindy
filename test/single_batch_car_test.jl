5077523c22705a5560eb570c5347e19f1dc5411f
Junette Hsin <junetter@gmail.com>
2024-03-14 17:38:30 -0500

    created df_mean_err_all.csv

--- test/single_batch_car_test.jl
+++ test/single_batch_car_test.jl
@@ -30,9 +30,9 @@ end
 
 freq_hz = 50 
 
-for noise = 0.01 : 0.01 : 0.1 
+for noise = 0.01 : 0.01 : 0.02 
 
-    for σn = [ 0.01, 0.02, 0.1, 0.2 ]
+    for σn = [ 0.01, 0.02, 0.1, 0.2 ] 
         
         opt_σn  = false  
         df_min_err_csvs_sindy, df_min_err_csvs_gpsindy, df_mean_err = cross_validate_csv_path( freq_hz, noise, σn, opt_σn ) 
@@ -45,86 +45,31 @@ for noise = 0.01 : 0.01 : 0.1
 end 
 
 
-
-
-
-
-
-
-
 ## ============================================ ##
-# ok, this is taking too long ... let's just break sindy 
+## ============================================ ##
+# let's compile all the stats of the files that I just made 
 
-freq_hz  = 50 
-noise    = 0.02 
-csv_path = string("test/data/jake_car_csvs_ctrlshift_no_trans/", freq_hz, "hz_noise_", noise, "/" )
+csv_path_up = string( "test/results/jake_car_csvs_ctrlshift_no_trans/" ) 
+csv_path_vec = readdir( csv_path_up ) 
 
-# create save path 
-csv_files_vec, save_path, save_path_fig = mkdir_save_path( csv_path ) 
-
-λ_vec = λ_vec_fn() 
-
-min_train_err_hist = [ 0.0 0.0 0.0 ] 
-for i in eachindex(csv_files_vec) 
+header = [ "freq_hz", "noise", "σn", "opt_σn", "mean_err_sindy_train", "mean_err_gpsindy_train", "mean_err_sindy_test", "mean_err_gpsindy_test" ]
+df_mean_err_all = DataFrame( fill( [], 8 ), header ) 
+for i = eachindex(csv_path_vec) 
 # i = 1 
-    
-    # extract data 
-    data_train, data_test = car_data_struct( csv_files_vec[i] ) 
 
-    sindy_train_err_hist = [] 
-    sindy_test_err_hist  = [] 
-    for i_λ = eachindex( λ_vec ) 
+    csv_path = string( csv_path_up, csv_path_vec[i] ) 
+    csv_path_df = string( csv_path, "/df_mean_err.csv" )
 
-        λ = λ_vec[i_λ] 
-
-        # get sizes 
-        x_vars, u_vars, poly_order, n_vars = size_x_n_vars( data_train.x_noise, data_train.u ) 
-        
-        # get x0 from noisy and smoothed data 
-        x0_train    = data_train.x_noise[1,:]  
-        x0_test     = data_test.x_noise[1,:]  
-        
-        # ----------------------- # 
-        # SINDY-lasso ! 
-        Ξ_sindy = sindy_lasso( data_train.x_noise, data_train.dx_noise, λ, data_train.u ) 
-        
-        # integrate discovered dynamics 
-        dx_fn_sindy   = build_dx_fn( poly_order, x_vars, u_vars, Ξ_sindy ) 
-        x_sindy_train = integrate_euler( dx_fn_sindy, x0_train, data_train.t, data_train.u ) 
-        x_sindy_test  = integrate_euler( dx_fn_sindy, x0_test, data_test.t, data_test.u ) 
-
-        # save error norm 
-        sindy_train_err = norm( x_sindy_train - data_train.x_noise ) 
-        sindy_test_err  = norm( x_sindy_test - data_test.x_noise ) 
-        push!( sindy_train_err_hist, sindy_train_err ) 
-        push!( sindy_test_err_hist,  sindy_test_err  ) 
-
+    # read in the csv 
+    if isfile( csv_path_df )
+        df_mean_err = CSV.read( csv_path_df, DataFrame ) 
+        push!( df_mean_err_all, df_mean_err[1,:] )             
     end 
 
-    # save index of minimum error 
-    i_min_err = argmin( sindy_train_err_hist ) 
-    min_train_err = [ λ_vec[i_min_err] sindy_train_err_hist[i_min_err] sindy_test_err_hist[i_min_err] ] 
-    # push!( min_train_err_hist, min_train_err ) 
-    min_train_err_hist = [ min_train_err_hist ; min_train_err ] 
-    
 end 
 
-min_train_err_hist = min_train_err_hist[2:end,:] 
+# save the dataframe 
+CSV.write( "df_mean_err_all.csv", df_mean_err_all) 
 
-## ============================================ ##
-
-using CSV 
-
-for i in eachindex( csv_files_vec )
-    csv_files_vec[i] = replace( csv_files_vec[i], csv_path => "" ) 
-end 
-
-header = [ "csv_file", "λ_min", "min_train_err", "--> test_err" ]
-data   = [ csv_files_vec min_train_err_hist ] 
-df = DataFrame( data, header ) 
-
-# save to csv 
-csv_save = string( "min_train_err_hist.csv" ) 
-CSV.write( csv_save, df ) 
 
 
